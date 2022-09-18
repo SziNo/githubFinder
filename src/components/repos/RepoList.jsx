0419f964fb59986c094f5b9585a136d5c05ef3f8
SziNo <szino1848@gmail.com>
2022-09-18 13:18:12 +0200

    fourth commit

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -7,9 +7,9 @@ const RepoList = ({ repos }) => {
       <div className='card-body'>
         <h2 className='text-3xl my-4 font-bold card-title'>Repositories</h2>
         <h2>
-          {repos.map((repo) => (
-            <RepoItem key={repo.id} repo={repo} />
-          ))}
+          {repos
+            .map((repo) => <RepoItem key={repo.id} repo={repo} />)
+            .slice(0, 5)}
         </h2>
       </div>
     </div>
